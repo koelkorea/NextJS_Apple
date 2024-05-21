24000d4ff00ca5a8945e8bb153196307ff4527fe
LSH <koelkorea@gmail.com>
2024-05-22 03:57:09 +0900

    2nd commit
    기초코드 갱신

--- grocery/app/page.js
+++ grocery/app/page.js
@@ -4,7 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev kim</p>
     </div>
   );
 }
